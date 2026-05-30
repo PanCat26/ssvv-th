b6e71c48f9e4a29852796176ca159a532cf0877b
PanCat26 <catalin.pangaleanu@gmail.com>
2026-05-30 18:22:24 +0300

    Add project scaffolding

--- Program.cs
+++ Program.cs
@@ -1,15 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using ssvv_th.Data;
+using ssvv_th.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// MVC
 builder.Services.AddControllersWithViews();
 
+// Database (MySQL via Pomelo)
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// Application services (one per entity, no repository layer)
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<ILoanService, LoanService>();
+
 var app = builder.Build();
+
+// Create the database/schema on startup if it does not exist yet.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Book/Index");
     app.UseHsts();
 }
 
@@ -20,8 +42,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Books page is the default landing page.
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Book}/{action=Index}/{id?}");
 
 app.Run();
